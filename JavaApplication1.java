9ea00ee616a8f48588b03804964aeb46217f8d55
CristianAlfaro <37422663+CristianAlfaro@users.noreply.github.com>
2018-03-15 19:52:01 -0600

    Add files via upload

---
+++ JavaApplication1.java
@@ -0,0 +1,21 @@
+
+package javaapplication1;
+
+/**
+ *
+ * @author Onilink
+ */
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nombre: Cristian Mauricio");
+        System.out.println("Apellidos: Alfaro Contreras");
+        System.out.println("Edad: 18 años");
+        System.out.println("Fecha de nacimiento: 18 de sep de 2018");
+        System.out.println("Carnet: 00167917");
+        System.out.println("Tarjeta de banco: ***************** te la creiste we :v");
+        
+    }
+    
+}
+
